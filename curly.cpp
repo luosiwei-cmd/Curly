d695f0b155bb0d05f5f9600f0ca8133fcfd3a1ef
luosw <61175380+luosiwei-cmd@users.noreply.github.com>
2020-08-14 09:26:53 +0800

    init!

---
+++ curly.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+string temp;
+int    hcnt;
+int    main(int argc, char** argv) {
+    ifstream fin(argv[1]);
+    ofstream fout(argv[2]);
+    fout << "<!doctype html>\n<html>\n<head>\n<meta charset = 'UTF-8'><meta "
+            "name = "
+            "'viewport' content ='width=device-width"
+            "initial-scale=1'>\n<title>CURLY</title>\n";
+    fout << "<link rel=\"stylesheet\" type=\"text/css\" href=\"mystyles.css\" "
+            "media=\"screen\" />\n";
+    fout << "</head>\n";
+    fout << "<body class='typora-export os-windows'>\n<div id ='write' class = "
+            "'is-node'>";
+
+    fout << "</body>\n</html>\n";
+    return 0;
+}
